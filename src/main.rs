f67cda0293330234017d748eefa9b47584db04cf
Sunyb01 <18911031529@189.cn>
2024-03-18 17:25:56 +0800

    perf(*): 导入优化

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ mod router;
 mod service;
 
 use configs::APP_CONFIG;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let _ = APP_CONFIG.get_or_init(|| configs::load_properties());
     let app = router::init_and_regist();
-    let listener =
-        tokio::net::TcpListener::bind(APP_CONFIG.get().unwrap().server.clone().get_addr())
-            .await
-            .unwrap();
+    let listener = TcpListener::bind(APP_CONFIG.get().unwrap().server.clone().get_addr())
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
