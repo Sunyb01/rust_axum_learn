1502e341d7ebea57a573f0cfb624fac6198f80ab
Sunyb01 <18911031529@189.cn>
2024-08-09 10:38:50 +0800

    doc(*): web模块文档

--- web/src/main.rs
+++ web/src/main.rs
@@ -10,6 +10,10 @@ use configs::APP_CONFIG;
 use server;
 use tokio::net::TcpListener;
 
+// axum 这个库目前在linux环境上, 会出现内存泄漏的问题
+// 主要原因是因为 linux gun libc;
+// 可以使用 musl编译, 也可以使用三方的内存分配器
+// 文章地址: https://rustcc.cn/article?id=61e5dd0a-45c6-4f09-8554-c6b7c01f202b
 #[tokio::main]
 async fn main() {
     web::web_lib_hello();
