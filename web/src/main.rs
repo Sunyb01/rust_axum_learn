6dafa1709a1bd91dfcb4154207063001290c8401
Sunyb01 <18911031529@189.cn>
2024-09-12 09:56:15 +0800

    feat(*): router merge

--- web/src/main.rs
+++ web/src/main.rs
@@ -6,6 +6,7 @@ mod router;
 mod service;
 
 use app;
+use axum::Router;
 use configs::APP_CONFIG;
 use server;
 use tokio::net::TcpListener;
@@ -20,7 +21,8 @@ async fn main() {
     app::hello_web();
     server::hello();
     let _ = APP_CONFIG.get_or_init(|| configs::load_properties());
-    let app = router::init_and_register_router();
+    // 使用merge合并多个router
+    let app = Router::new().merge(router::init_and_register_router());
     let listener = TcpListener::bind(APP_CONFIG.get().unwrap().server.clone().get_addr())
         .await
         .unwrap();
